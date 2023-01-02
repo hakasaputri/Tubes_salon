a3e41740be4c00156918565c75dc7ff9d8e96c86
Listia Ningrum <listianingrum2002@upi.edu>
2023-01-03 02:02:26 +0700

    help commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:tubes_provis/Pages/LoginRegister/LoginForm.dart';
 import 'package:tubes_provis/Pages/LoginRegister/SignupForm.dart';
 import 'package:tubes_provis/Pages/LoginRegister/Logo.dart';
+import 'package:tubes_provis/Pages/Help/HelpForm.dart';
 import 'package:tubes_provis/utils.dart';
 import 'package:tubes_provis/Pages/LoginRegister/Get_started.dart';
-
-
 
 void main() {
   runApp(const MyApp());
@@ -14,8 +13,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,29 +30,21 @@ class MyApp extends StatelessWidget {
           '/login' : (contex) => LoginForm(),
           '/get_started' : (contex) => Get_started(),
           '/' : (contex) => Logo(),
-
+          '/help' : (contex) => HelpForm(),
         },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      // appBar: AppBar(
-      //
-      //   title: Text(widget.title),
-      // ),
       body: LoginForm()
     );
   }
