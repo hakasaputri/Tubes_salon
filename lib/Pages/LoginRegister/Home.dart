1e23647308cf6b57783f74a35c3cf4c47e30fc7d
Listia Ningrum <95666745+listianingrum16@users.noreply.github.com>
2023-01-03 17:18:31 +0700

    Update Home.dart

--- lib/Pages/LoginRegister/Home.dart
+++ lib/Pages/LoginRegister/Home.dart
@@ -69,6 +69,71 @@ class _HomeState extends State<Home> {
           backgroundColor: Color(0xffffb0b0),
           // title: Text('Hi, Jessica Pauline'),
         ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(color: Color(0xffffffff)),
+                child: Row(
+                  children: [
+                    SizedBox(width: 10),
+                    CircleAvatar(
+                      backgroundImage: NetworkImage("http://medialengka.com/profile.jpg"),
+                      maxRadius: 30,
+                    ),
+                    SizedBox(width: 20),
+                    Text("username",
+                        style: TextStyle(fontSize: 15, color: Colors.white)
+                    )
+                  ],
+                ),
+              ),
+              ListTile(
+                  leading: const Icon(Icons.house),
+                  title: Text("Home"),
+                  onTap: (){Navigator.of(context).pushNamed("/home");}
+              ),
+              ListTile(
+                  leading: const Icon(Icons.person_outline),
+                  title: Text("Profile"),
+                  onTap: (){Navigator.of(context).pushNamed("/profile");}
+              ),
+              ListTile(
+                  leading: const Icon(Icons.calculate),
+                  title: Text("Treatment"),
+                  onTap: (){Navigator.of(context).pushNamed("/treatment");}
+              ),
+               ListTile(
+                  leading: const Icon(Icons.calculate),
+                  title: Text("Notifications"),
+                  onTap: (){Navigator.of(context).pushNamed("/notification");}
+              ),
+               ListTile(
+                  leading: const Icon(Icons.calculate),
+                  title: Text("About us"),
+                  onTap: (){Navigator.of(context).pushNamed("/about");}
+              ),
+               ListTile(
+                  leading: const Icon(Icons.calculate),
+                  title: Text("Help"),
+                  onTap: (){Navigator.of(context).pushNamed("/help");}
+              ),
+              Container(
+                  child: Align(
+                      alignment: FractionalOffset.bottomCenter,
+                      child: Column(
+                        children: <Widget>[
+                          Divider(),
+                          ListTile(
+                              leading: Icon(Icons.logout),
+                              title: Text('Log out'))
+                        ],
+                      )
+                    )
+                  ),
+            ],
+          ),
+        ),
         body: Container(
           child: Column(
             children: <Widget>[
